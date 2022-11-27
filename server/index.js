58d38fecd1374ea4fdfc8364459372866237f97a
OlegAnisimov <gramafon@yandex.ru>
2022-11-27 14:50:55 +0300

    create PUT to update specific todo item

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.get('/todos', async (req, res) => {
 app.get('/todos/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const todo = await pool.query('SELECT * FROM todo WHERE todo_id=$1',
+        const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1',
             [id]);
         res.json(todo.rows[0]);
     } catch (e) {
@@ -44,7 +44,17 @@ app.get('/todos/:id', async (req, res) => {
 });
 
 // update a todo_item
-
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2',
+            [description, id]);
+        res.json('Todo was updated');
+    } catch (e) {
+        console.error(e.message);
+    }
+});
 //delete a todo_item
 
 
