53a8010cc7cecc8b5d3d29cf87cbb38ca88e59f6
OlegAnisimov <gramafon@yandex.ru>
2022-11-27 13:04:05 +0300

    create connection to DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db')
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+// ROUTES
+
+//create a todo_item (use _item that not sign todo)
+
+//get all todos
+
+// get a todo_item
+
+// update a todo_item
+
+//delete a todo_item
+
 
 app.listen(5000, () => {
     console.log('Server has started on port 5000');
